0c39bb702132698edd05e49146fbcf8ad7f6a36a
Adam Chelminski <chelminski.adam@gmail.com>
2015-04-26 23:46:31 -0400

    Added basic blog webapp

---
+++ example_webapps/blog/blog_app.cpp
@@ -0,0 +1,43 @@
+#include <micro/app.hpp>
+#include <micro/response.hpp>
+#include <micro/request.hpp>
+#include <sstream>
+#include "blog_entry.hpp"
+
+std::vector<blog_entry> entries;
+
+void homepage(const micro::request &req, micro::response &resp) {
+    std::ostringstream page;
+    page << "<html><body><h1>Welcome to the blog!!</h1>";
+
+    page << "<a href=\"/new\">New Entry</a><br/>";
+
+    for (auto entry = entries.rbegin(); entry != entries.rend(); ++entry)
+        page << *entry;
+
+    page << "</body></html>";
+
+    resp.render_string(page.str());
+}
+
+void new_entry(const micro::request &req, micro::response &resp) {
+    if(req.get_method() == "POST") {
+        blog_entry new_entry{req.get_post_param("title"), req.get_post_param("body"), req.get_post_param("name")};
+        entries.push_back(new_entry);
+        resp.redirect("/");
+    }
+    else {
+        resp.render_file("new_entry.html");
+    }
+}
+
+int main(int argc, char **argv) {
+    micro::app app;
+
+    app.set_pool_size(8);
+
+    app.add_route("/", homepage);
+    app.add_route("/new", new_entry);
+
+    app.run();
+}
