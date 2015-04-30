7b8bdb891c39b75d5805bfd5cd9a8fb448e5f49f
mgouzenko <mgouzenko@gmail.com>
2015-04-30 02:33:47 -0400

    fixed tests that won't compile

--- example_webapps/module_test/module_test.cpp
+++ example_webapps/module_test/module_test.cpp
@@ -11,83 +11,30 @@
 micro::response hello(const micro::request& req)
 {
     micro::response res;
-    time_t t = time(0) + 100;
     res.render_string("hello world");
-    micro::Cookie c = micro::Cookie("fifth", "5", t, "/hello");
-    res.set_cookie(c);
     return res;
 }
 
-micro::response test_redirect(const micro::request& req)
+micro::response test_module_redirect(const micro::request& req)
 {
     micro::response res;
-    res.redirect("/other", true);
+    res.redirect("/hello", true);
     return res;
 }
 
-micro::response other(const micro::request& req)
+micro::response goodbye(const micro::request& req)
 {
     micro::response res;
-    res.render_string("you have been redirected");
-    return res;
-}
-
-micro::response bad_url(const micro::request& req)
-{
-    micro::response res;
-    res.render_status(503);
-    return res;
-}
-
-micro::response get_stuff(const micro::request& req)
-{
-    micro::response res;
-    auto param1 = req.get_get_param("param1");
-    auto param2 = req.get_get_param("param2");
-    auto param3 = req.get_get_param("param3");
-    res.render_string("<html><body>param1 : " + param1 + "<br/>param2 : " + param2 + "<br/>param3 : " + param3);
-    return res;
-}
-
-micro::response bad_url_custom(const micro::request& req)
-{
-    micro::response res;
-    res.render_status(501, "Sorry Bro, its Broken");
-    return res;
-}
-
-micro::response nothing(const micro::request& req)
-{   micro::response res;
-    //User returns nothing
-    //Browser will stay on same page
-    return res;
-}
-
-micro::response serve_number(const micro::request& req)
-{
-    micro::response res;
-    res.render_string("User ID requested: " + req.label_values.at("id"));
+    res.render_string("bye!");
     return res;
 }
 
 int main(int argc, char** argv){
-    if(argc != 2) {
-        std::cout << "Usage: test_app <static_file_root>\n";
-        exit(1);
-    }
-
     micro::app application;
-    application.set_pool_size(8);
-    application.set_static_root(argv[1]);
     micro::module mod = { {"/hello", hello},
-                          {"/test_redirect", test_redirect},
-                          {"/other", other},
-                          {"/bad_url", bad_url},
-                          {"/nothing", nothing},
-                          {"/bad_url_custom", bad_url_custom},
-                          {"/user/<int:id>/profile", serve_number},
-                          {"/get_stuff", get_stuff}
+                          {"/test_module_redirect", test_module_redirect}
                         };
     application.add_module(mod, "/module");
+    application.add_route("/goodbye", goodbye);
     application.run();
 }
--- example_webapps/timeout_test/timeout_test.cpp
+++ example_webapps/timeout_test/timeout_test.cpp
@@ -5,32 +5,18 @@
 #include <ctime>
 #include <fstream>
 
-micro::response excep(const micro::request& req){
-    micro::response res;
-    throw std::exception();
-    return res;
-}
-
-mirco::response hello(const micro::request& req)
+micro::response timeout(const micro::request& req)
 {
     micro::response res;
-    time_t t = time(0) + 100;
-    res.render_string("hello world");
-    micro::Cookie c = micro::Cookie("fifth", "5", t, "/hello");
-    res.set_cookie(c);
     while(1){
         std::this_thread::sleep_for(std::chrono::seconds(5));
     }
+    res.render_string("should not be reached"); 
     return res;
 }
 
 int main(){
-
-    micro::app application("8000");
-    application.toggle_debug_mode();
-    application.toggle_logging_mode();
-    application.set_pool_size(8);
-    application.add_route("/hello", hello);
-    application.add_route("/excep", excep);
+    micro::app application;
+    application.add_route("/timeout", timeout);
     application.run();
 }
