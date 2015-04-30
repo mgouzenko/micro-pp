6924bb7c1ba4c356fe0907328936d1b92628a23c
mgouzenko <mgouzenko@gmail.com>
2015-04-30 17:10:58 -0400

    removed unecessary comments

--- example_webapps/tutorial/basic_bjarneblog/blog_app.cpp
+++ example_webapps/tutorial/basic_bjarneblog/blog_app.cpp
@@ -30,6 +30,7 @@ micro::response homepage(const micro::request &req) {
     resp.render_string(page.str());
     return resp;
 }
+
 micro::response new_entry(const micro::request &req) {
     micro::response resp;
     blog_entry new_entry{req.get_post_param("title"), req.get_post_param("body"), req.get_post_param("name")};
@@ -37,41 +38,6 @@ micro::response new_entry(const micro::request &req) {
     resp.redirect("/");
     return resp;
 }
-
-/* micro::response homepage(const micro::request &req) { */
-/*     micro::response resp; */
-/*     std::ostringstream page; */
-/*     page << "<html><body><h1>Welcome to the blog!!</h1>"; */
-/*     page << "<a href=\"/new\">New Entry</a><br/>"; */
-/*     for (auto entry = entries.rbegin(); entry != entries.rend(); ++entry) */
-/*         page << *entry; */
-/*     page << "</body></html>"; */
-/*     resp.render_string(page.str()); */
-/*     return resp; */
-/* } */
-
-
-/* micro::response new_entry(const micro::request &req) { */
-/*     micro::response resp; */
-/*     if(req.get_method() == "POST") { */
-/*         blog_entry new_entry{req.get_post_param("title"), req.get_post_param("body"), req.get_post_param("name")}; */
-/*         entries.push_back(new_entry); */
-/*         resp.redirect("/"); */
-/*         return resp; */
-/*     } */
-/*     else { */
-/*         resp.render_file("new_entry.html"); */
-/*         return resp; */
-/*     } */
-/* } */
-
-/* micro::response new_entry(const micro::request &req) { */
-/*     micro::response resp; */
-/*     blog_entry new_entry{req.get_post_param("title"), req.get_post_param("body"), req.get_post_param("name")}; */
-/*     entries.push_back(new_entry); */
-/*     resp.redirect("/"); */
-/*     return resp; */
-/* } */
 
 int main(int argc, char **argv) {
     micro::app app("3000"); 
